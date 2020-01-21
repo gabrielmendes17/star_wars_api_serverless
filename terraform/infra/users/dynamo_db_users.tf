fe1f726a5e0a563de574f3ddfca22ec0f5bb58e5
Gabriel de Moraes Mendes <f570706@corp.caixa.gov.br>
2020-01-21 15:47:48 -0300

    [terraform]: Creating ssm parameter to reference table name

--- terraform/infra/users/dynamo_db_users.tf
+++ terraform/infra/users/dynamo_db_users.tf
@@ -9,3 +9,9 @@ resource "aws_dynamodb_table" "planets" {
   write_capacity = "${var.write_capacity}"
   read_capacity = "${var.read_capacity}"
 }
+
+resource "aws_ssm_parameter" "dynamodb_planets_table" {
+  name = "${var.environment}-dynamodb-planets-table"
+  type = "String"
+  value = "${aws_dynamodb_table.planets.name}"
+}
