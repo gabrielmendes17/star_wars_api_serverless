b5003eed58813480ae6330a7ff8cb93b1797950a
Gabriel de Moraes Mendes <f570706@corp.caixa.gov.br>
2020-01-21 16:40:06 -0300

    [terraform]: Adding policy to allow create logs on cloud watch

--- terraform/infra/planets/iam-policy-create.tf
+++ terraform/infra/planets/iam-policy-create.tf
@@ -13,6 +13,15 @@ resource "aws_iam_policy" "create_policy" {
       "Effect": "Allow",
       "Resource": "${aws_dynamodb_table.users.arn}"
     },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+      ],
+      "Resource": "*"
+    }
   ]
 }
 EOF
